d1aa278964d8c4e7d20f05a5cf499d2f138cfeac
tstloc01-1059 <31376427+tstloc01-1059@users.noreply.github.com>
2025-07-27 21:49:30 +0100

    added a lot of stuff and now it doesn't make sense anymore

--- script.js
+++ script.js
@@ -3,11 +3,17 @@
 //a way to compare both
 //a way to track rounds
 
+// ## VARIABLES ##
+
+// let computerChoice = ''
+// let humanChoice = prompt('What shall your hand be?') //user input of string "rock, paper, scissors"
+// let computerScore = 0
+// let humanScore = 0
+
 // ## COMPUTER ##
 
-let computerChoice = ''
-
 function getComputerChoice() {
+    let computerChoice = ''
     let result = Math.floor(Math.random() * 3) //create random number between 0 and 2
     switch (result) { //assign 0,1,2 a name rock, paper, scissors
         case 0:
@@ -24,16 +30,58 @@ function getComputerChoice() {
     }
 
     return computerChoice //display the outcome as an alert
-    
+
 }
 
 console.log(getComputerChoice())
 
-//## USER ##
+// //## USER ##
 
-let userChoice = prompt('What shall your hand be?') //user input of string "rock, paper, scissors"
-console.log(userChoice)
+// function getHumanChoice() {
+//     return humanChoice
+// }
 
+// // ## ROUND 
+// // ## 
+
+// function playRound(humanChoice, computerChoice) {
+//     // humanChoice = humanChoice.toLowerCase()
+//     // computerChoice = computerChoice.toLowerCase()
+
+//     switch (true) {
+//         case humanChoice === 'rock' && computerChoice === 'scissors':
+//             console.log('HUMAN WINS');
+//             break;
+//         case humanChoice === 'scissors' && computerChoice === 'rock':
+//             console.log('MACHINE WINS');
+//             break;
+
+//     }
+
+//     // if (humanChoice == 'rock' && computerChoice == 'scissors') {
+//     //     console.log('HUMAN WINS')
+//     // } else if (humanChoice == 'scissors' && computerChoice == 'rock') {
+//     //     console.log('MACHINE WINS')
+//     // } else if (humanChoice == 'paper' && computerChoice == 'rock') {
+//     //     console.log('HUMAN WINS')
+//     // } else if (humanChoice == 'rock' && computerChoice == 'paper') {
+//     //     console.log('MACHINE WINS')
+//     // } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+//     //     console.log('HUMAN WINS')
+//     // } else if (humanChoice == 'paper' && computerChoice == 'scissors') {
+//     //     console.log('MACHINE WINS')
+//     // } else {
+//     //     console.log('whatever')
+//     // }
+// }
+
+// const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
+
+// console.log(getHumanChoice())
+// console.log(getComputerChoice().toLowerCase() + '***')
+
+// playRound(humanSelection, computerSelection);
 
 //send it to the script
 //activate COMPUTER player randomiser number
